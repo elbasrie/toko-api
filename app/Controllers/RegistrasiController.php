3712bbc3eb05634ffb2c41f73f4215ef9c3f930a
elbasrie <ismailhasan1337@gmail.com>
2022-04-19 15:46:19 +0700

    toko-api

---
+++ app/Controllers/RegistrasiController.php
@@ -0,0 +1,31 @@
+<?php 
+namespace App\Controllers;
+use App\Models\MRegistrasi;
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\RESTful\ResourceController;
+
+
+class RegistrasiController extends ResourceController
+{
+    public $format = 'json';
+    protected function responseHasil($code, $status, $data)
+    {
+        return $this->respond([ 
+            'code' => $code,
+            'status' => $status,
+            'data' => $data,
+        ]);
+    }
+ // create
+    public function registrasi() {
+        $data = [
+            'name' => $this->request->getVar('name'),
+            'email'  => $this->request->getVar('email'),
+            'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+        ]; 
+        $model = new MRegistrasi();
+        $model->insert($data); 
+         return $this-> responseHasil(200,true, "Registrasi Berhasil");
+    }
+   
+}
